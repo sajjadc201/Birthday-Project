7c9f1aa9259634dc4c3e8fb21fda570c9bfda000
MUHAMMAD SAJJAD <sajjadc201@gmail.com>
2022-09-12 22:41:40 +0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,40 +1,41 @@
 const data = [
-    {
-      id: 1,
-      name: 'Bertie Yates',
-      age: 29,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
-    },
-    {
-      id: 2,
-      name: 'Hester Hogan',
-      age: 32,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
-    },
-    {
-      id: 3,
-      name: 'Larry Little',
-      age: 36,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
-    },
-    {
-      id: 4,
-      name: 'Sean Walsh',
-      age: 34,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-    },
-    {
-      id: 5,
-      name: 'Lola Gardner',
-      age: 29,
-      image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-    },
-  ];
+  {
+    id: 1,
+    name: 'Sajjad',
+    age: 33,
+    image:
+      'https://i.postimg.cc/X7BvMN9K/IMG20191202065958.jpg',
+  },
+  {
+    id: 2,
+    name: 'Samreen',
+    age: 32,
+    image:
+      'https://i.postimg.cc/QCZV0HMd/Screenshot-3.png',
+  },
+  {
+    id: 3,
+    name: 'Ariyan',
+    age: 12,
+    image:
+      'https://i.postimg.cc/gJ3fy5hZ/IMG20191128093509.jpg',
+  },
+  {
+    id: 4,
+    name: 'Arfa',
+    age: 10,
+    image:
+      'https://i.postimg.cc/kXt7Ynb0/Screenshot-2.png',
+  },
+  {
+    id: 5,
+    name: 'Ahmad',
+    age: 5,
+    image:
+      'https://i.postimg.cc/MZR0Ww0w/IMG20200310171522.jpg',
+  },
+];
+
   
 
   export default data
